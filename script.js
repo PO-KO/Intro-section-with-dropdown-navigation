183966686b759ec9e6595e6f3b4a8dfcd1f5f556
PO-KO <ayoub18daoui@gmail.com>
2022-05-20 21:09:53 +0100

    I repeat everything

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-let burger = document.querySelector("burger");
-let close = document.querySelector("close");
+let burger = document.querySelector(".burger");
+let close = document.querySelector(".close");
+let nav = document.querySelector("header nav");
 let dropdownOne = document.querySelector("header nav .navbar > li:first-child");
 let dropdownTwo = document.querySelector(
   "header nav .navbar > li:nth-child(2)"
 );
-
-console.log(dropdownTwo);
+let overlay = document.querySelector(".overlay");
+burger.onclick = () => {
+  nav.classList.add("open");
+  nav.parentElement.style.zIndex = 10;
+  overlay.style.display = "block";
+};
+close.onclick = () => {
+  nav.classList.remove("open");
+  setTimeout(() => {
+    nav.parentElement.style.zIndex = -10;
+  }, 300);
+  overlay.style.display = "none";
+};
 
 dropdownOne.onclick = expand;
 dropdownTwo.onclick = expand;
 
 function expand() {
-  this.querySelector("a").classList.toggle("rotate");
+  this.querySelector("svg").classList.toggle("rotate");
   this.lastElementChild.classList.toggle("expand");
 }

92d169d847aa3419308463ea1adc33f62574042d
PO-KO <ayoub18daoui@gmail.com>
2022-05-22 18:54:15 +0100

    Finish All js script

--- script.js
+++ script.js
@@ -19,10 +19,70 @@ close.onclick = () => {
   overlay.style.display = "none";
 };
 
-dropdownOne.onclick = expand;
-dropdownTwo.onclick = expand;
+dropdownOne.onclick = showDrop;
+dropdownTwo.onclick = showDrop;
 
-function expand() {
-  this.querySelector("svg").classList.toggle("rotate");
-  this.lastElementChild.classList.toggle("expand");
+if (window.innerWidth < 920) {
+  dropdownOne.lastElementChild.style.display = "block";
+  console.log(dropdownOne.lastElementChild.style.display);
+  console.log(dropdownOne.lastElementChild);
+  dropdownTwo.lastElementChild.style.display = "block";
+  nav.parentElement.style.zIndex = -10;
+} else {
+  dropdownOne.lastElementChild.style.display = "none";
+  dropdownTwo.lastElementChild.style.display = "none";
+  overlay.style.display = "none";
+  nav.classList.remove("open");
+  nav.parentElement.style.zIndex = 10;
 }
+
+window.onresize = function () {
+  if (this.innerWidth < 920) {
+    dropdownOne.lastElementChild.style.display = "block";
+    dropdownTwo.lastElementChild.style.display = "block";
+    nav.parentElement.style.zIndex = -10;
+  } else {
+    dropdownOne.lastElementChild.style.display = "none";
+    dropdownTwo.lastElementChild.style.display = "none";
+    overlay.style.display = "none";
+    nav.classList.remove("open");
+    nav.parentElement.style.zIndex = 10;
+  }
+};
+function showDrop() {
+  if (window.innerWidth < 920) {
+    // this.lastElementChild.style.display = "block";
+    this.lastElementChild.classList.remove("hover");
+    this.querySelector("svg").classList.toggle("rotate");
+    this.lastElementChild.classList.toggle("expand");
+  } else {
+    if (this.lastElementChild.classList.contains("hover")) {
+      this.lastElementChild.classList.remove("hover");
+      setTimeout(() => {
+        this.lastElementChild.style.display = "none";
+      }, 300);
+    } else {
+      this.lastElementChild.style.display = "block";
+      setTimeout(() => {
+        this.lastElementChild.classList.add("hover");
+      }, 100);
+    }
+  }
+}
+
+window.addEventListener("click", function (event) {
+  if (
+    event.target.parentElement.lastElementChild !==
+      dropdownOne.lastElementChild &&
+    event.target.parentElement.lastElementChild !==
+      dropdownTwo.lastElementChild &&
+    window.innerWidth >= 920
+  ) {
+    dropdownOne.lastElementChild.style.display = "none";
+    dropdownOne.lastElementChild.classList.remove("hover");
+    dropdownTwo.lastElementChild.style.display = "none";
+    dropdownTwo.lastElementChild.classList.remove("hover");
+  }
+});
+
+// When the user clicks anywhere outside of the modal, close it
